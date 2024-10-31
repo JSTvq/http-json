5da7bc30ec4faf96481ea35dc219bfaa11309351
JSTvq <kir8530@yandex.ru>
2024-10-31 22:51:14 +0700

    обновление проекта

--- src/main/java/com/kir138/task1/service/HibernateWeatherService.java
+++ src/main/java/com/kir138/task1/service/HibernateWeatherService.java
@@ -1,13 +1,45 @@
 package com.kir138.task1.service;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import com.kir138.task1.mapper.WeatherHistoryMapper;
+import com.kir138.task1.model.dto.CityDto;
+import com.kir138.task1.model.entity.WeatherHistory;
+import com.kir138.task1.repository.WeatherCityHibernateRepository;
+import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 public class HibernateWeatherService {
+    private final WeatherCityHibernateRepository weatherCityHibernateRepository;
+    private final WeatherHistoryMapper weatherHistoryMapper;
 
-    private static final SessionFactory sessionFactory = new Configuration()
-            .configure("hibernate.cfg.xml")
-            .buildSessionFactory();
+    public CityDto findCityById(Long id) {
+        return weatherCityHibernateRepository.findById(id)
+                .map(weatherHistoryMapper::toCityDto)
+                .orElseThrow(() -> new IllegalArgumentException("город с таким Id не найден"));
+    }
+
+    public List<CityDto> findCityByName(String cityName) {
+        return weatherCityHibernateRepository.findByNameCity(cityName)
+                .stream()
+                .map(weatherHistoryMapper::toCityDto)
+                .toList();
+    }
+
+    public List<CityDto> findAllCities() {
+        return weatherCityHibernateRepository.findAll()
+                .stream()
+                .map(weatherHistoryMapper::toCityDto)
+                .toList();
+    }
+
+    public CityDto save(WeatherHistory weatherHistory) {
+        return weatherHistoryMapper.toCityDto(weatherCityHibernateRepository.save(weatherHistory));
+    }
+
+    public void deleteCityById(Long id) {
+        weatherCityHibernateRepository.deleteCityById(id);
+    }
 
 
 }
